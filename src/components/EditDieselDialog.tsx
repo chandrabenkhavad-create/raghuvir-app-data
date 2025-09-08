4e98b0f1d4067fb8a3ecd895e3932424526fd363
CHANDRABEN KHAVAD <chandrabenkhavad@gmail.com>
2025-09-08 08:07:10 +0000

    Edit Sale Entry and Edit Diesel Entry ke popup window ki ui theme design

--- src/components/EditDieselDialog.tsx
+++ src/components/EditDieselDialog.tsx
@@ -6,7 +6,6 @@ import { useForm, type SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { 
-  Fuel, 
   Printer, 
   Save, 
   Car, 
